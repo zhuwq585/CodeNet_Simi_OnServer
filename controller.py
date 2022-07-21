894b4de7dc5eeed5681bdc0800cb523a1b386fdc
syu <syu@syu-MacBook-Pro.local>
2022-07-21 18:05:56 +0900

    problem list created

--- controller.py
+++ controller.py
@@ -1,27 +1,44 @@
-import os
+from concurrent.futures import thread
+import os, time
 from xml.etree.ElementTree import PI
 
+time_start = time.time()
 rootPath = "./"
 extName  = ".tsv"
+language = "Java"
+pIdListFile = language + "problemList.txt"
+threadNum = 5
 
-fileWalking = os.walk('./sourceData')
-splitedTmp = None
-fileUrl  = ''
-pId      = ''
-language = ''
+for pIdLine in open(pIdListFile,"r").readlines():
+    fileURL    = language + "/source/" + pIdLine[:-1] + "-tokens.tsv"
+    outputFile = language + "/result/" + pIdLine[:-1] + "-simi.csv"
+    outputlog  = language + "/result/" + pIdLine[:-1] + "-simi.log"  
+    print("Opertaing: " + fileURL)
+    os.system("java -jar codenetSimi.jar " + fileURL + " " + str(threadNum) + " " + outputFile + " > " + outputlog)
+  
+time_end = time.time()  
+with open(language+'/over.txt',"w") as f:
+    f.write("time: " + str(time_end-time_start))
+    f.close()
+    
+# fileWalking = os.walk('./sourceData')
+# splitedTmp = None
+# fileUrl  = ''
+# pId      = ''
+# language = ''
 
-for path, dir_list, file_list in fileWalking:  
-    for file_name in file_list: 
-        fileUrl = os.path.abspath(path + '/' + file_name)
-        if os.path.splitext(fileUrl)[1] == extName:
-            splitedTmp = fileUrl.split("/")
-            pId        = splitedTmp[-1].split("-")[0]
-            language   = splitedTmp[-2]
+# for path, dir_list, file_list in fileWalking:  
+#     for file_name in file_list: 
+#         fileUrl = os.path.abspath(path + '/' + file_name)
+#         if os.path.splitext(fileUrl)[1] == extName:
+#             splitedTmp = fileUrl.split("/")
+#             pId        = splitedTmp[-1].split("-")[0]
+#             language   = splitedTmp[-2]
             
-            outputFile = "./result/"+language+"/"+pId+"-simi.csv"
-            logFile    = "./result/"+language+"/"+pId+"-simi.log"
-            # os.system("java -jar codenetSimi.jar " + fileUrl + " 200 " + outputFile + " > " + logFile)
+#             outputFile = "./result/"+language+"/"+pId+"-simi.csv"
+#             logFile    = "./result/"+language+"/"+pId+"-simi.log"
+#             # os.system("java -jar codenetSimi.jar " + fileUrl + " 200 " + outputFile + " > " + logFile)
 
-            print("java -jar codenetSimi.jar " + fileUrl + " 200 " + outputFile + " > " + logFile)
-            # print(os.path.splitext(fileUrl))
+#             print("java -jar codenetSimi.jar " + fileUrl + " 200 " + outputFile + " > " + logFile)
+#             # print(os.path.splitext(fileUrl))
         
---
+++ problemListGeneration.py
@@ -0,0 +1,74 @@
+#####
+# author: zhuwq585
+# generate a unique problem list for each language.
+# for each problem cluster, the minist one is reserved
+####
+
+from logging import root
+import os , sys
+from xml.etree.ElementTree import PI
+
+PROBLEM_CLUSTER = "./identical_problem_clusters"
+LANGUAGE_LIST   = ["Java", "Python", "C", "C++"]
+ROOT            = "./"
+
+class Cluster:
+    def __init__(self):
+        self.problemArr = []
+        self.ifOver     = False
+        
+    def addProblemId(self, id):
+        self.problemArr.append(id)
+
+def getMinist(language, idArr):
+    minTmp = sys.maxsize
+    res    = None
+    for id in idArr:
+        size = len(open(ROOT + language + "/source/" + id + "-tokens.tsv", "r").readlines())
+        if size <= minTmp:
+            minTmp = size
+            res = id
+    
+    return str(res)
+
+def idFormat(idNum):
+    res = str(idNum)
+    
+    while len(res) < 5:
+        res = '0' + res
+    
+    return res
+
+def checkSize(language, pId):
+    return  len(open(ROOT+language+'/source/'+pId+"-tokens.tsv","r").readlines()) > 1
+
+## create cluster dict 
+splitTmp    = None
+clusterTmp  = None
+clusterDic  = dict()
+with open(PROBLEM_CLUSTER,"r") as file:
+    for clusterLine in file.readlines():
+        clusterTmp = Cluster()
+        splitTmp = clusterLine[:-1].split(",")
+        for id in splitTmp:
+            clusterDic[id] = clusterTmp
+            clusterTmp.addProblemId(id)
+            
+for language in LANGUAGE_LIST:
+    outputFile = open("./" + language + "problemList.txt", "w")
+    for idNum in range(0, 4053):
+        id = "p" + idFormat(idNum)
+        if id in clusterDic:
+            if clusterDic[id].ifOver:
+                continue
+            else:
+                minId = getMinist(language, clusterDic[id].problemArr)
+                if checkSize(language, minId):
+                    outputFile.write( minId + "\n")
+        else:
+            if checkSize(language, id):
+                outputFile.write(id + "\n")
+    outputFile.close()
+    
+       
+    
